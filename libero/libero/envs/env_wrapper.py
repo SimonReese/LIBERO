4ab6780b0e03adc6d3869c688715b7490830c219
SimonReese <59612841+SimonReese@users.noreply.github.com>
2026-03-17 13:26:11 +0100

    Adds type, print error info

--- libero/libero/envs/env_wrapper.py
+++ libero/libero/envs/env_wrapper.py
@@ -53,7 +53,7 @@ class ControlEnv:
         self.problem_name = problem_info["problem_name"]
         self.domain_name = problem_info["domain_name"]
         self.language_instruction = problem_info["language_instruction"]
-        self.env = TASK_MAPPING[self.problem_name](
+        self.env: bddl_base_domain.BDDLBaseDomain = TASK_MAPPING[self.problem_name](
             bddl_file_name,
             robots=robots,
             controller_configs=controller_configs,
